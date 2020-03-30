a5942f877e84bf4b3e2f40b5142dfc8097fd278f
Alexey Roik <alexey.roik@nebulagames.org>
2020-03-30 22:43:03 +0300

    Add empty iOS project

---
+++ ZenCoreGraphics/SupportingFiles/ZenCoreGraphics.h
@@ -0,0 +1,19 @@
+//
+//  ZenCoreGraphics.h
+//  ZenCoreGraphics
+//
+//  Created by Alexey Roik on 3/30/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenCoreGraphics.
+FOUNDATION_EXPORT double ZenCoreGraphicsVersionNumber;
+
+//! Project version string for ZenCoreGraphics.
+FOUNDATION_EXPORT const unsigned char ZenCoreGraphicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenCoreGraphics/PublicHeader.h>
+
+
