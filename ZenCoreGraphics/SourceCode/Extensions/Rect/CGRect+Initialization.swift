413d5278517c7f97dd8f63178f81b92b506ce3df
Alexey Roik <alexey.roik@nebulagames.org>
2020-03-30 22:58:51 +0300

    Add 'CGRect+Initialization' extension

---
+++ ZenCoreGraphics/SourceCode/Extensions/Rect/CGRect+Initialization.swift
@@ -0,0 +1,22 @@
+import CoreGraphics
+
+public extension CGRect {
+	
+	/**
+	Creates a rectangle with the given center and dimensions.
+	- parameter center: The center of the new rectangle
+	- parameter size: The dimensions of the new rectangle
+	*/
+	init(
+		center: CGPoint,
+		size: CGSize
+	) {
+		self.init(
+			x: center.x - size.width / 2,
+			y: center.y - size.height / 2,
+			width: size.width,
+			height: size.height
+		)
+	}
+	
+}
