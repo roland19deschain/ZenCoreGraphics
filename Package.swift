6a4f802982671c2d819b7a57c9465020753f9d22
Alexey Roik <alexey.roik@nebulagames.org>
2022-03-05 20:36:54 +0300

    Update 'Package'

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 		)
 	],
 	dependencies: [
-		.package(url: "https://github.com/roland19deschain/ZenSwift.git", from: "1.8.1"),
+		.package(url: "https://github.com/roland19deschain/ZenSwift.git", from: "2.0.0"),
 	],
 	targets: [
 		.target(
